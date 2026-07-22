ea94465edff8e2969b1739c7152b7062880adb3f
awakenedarpit <awakenedarpit@gmail.com>
2026-07-22 23:05:11 +0530

    Delete app.js

--- jss/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    console.log("🎂 A Gift Beyond Wishes Started");
-
-    document.title =
-        `Happy Birthday ${CONFIG.recipient.nickname} ❤️`;
-
-});
