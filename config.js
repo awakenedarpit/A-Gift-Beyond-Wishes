a238b460e814ad39c241f3cbf54f3134c7ef73b7
awakenedarpit <awakenedarpit@gmail.com>
2026-07-22 15:41:12 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,39 @@
+const CONFIG = {
+  recipient: {
+    name: "Mahima Kanoujiya",
+    nickname: "Mahima"
+  },
+
+  sender: {
+    name: "Arpit Raj"
+  },
+
+  birthday: {
+    date: "2026-08-13"
+  },
+
+  music: {
+    background: "assets/music/birthday.mp3"
+  },
+
+  gallery: [
+    {
+      image: "assets/photos/photo1.jpg",
+      caption: "A beautiful memory 🌸"
+    },
+    {
+      image: "assets/photos/photo2.jpg",
+      caption: "Smiles that make every day brighter ❤️"
+    },
+    {
+      image: "assets/photos/photo3.jpg",
+      caption: "One more unforgettable moment ✨"
+    }
+  ],
+
+  colors: {
+    primary: "#ff69b4",
+    secondary: "#c084fc",
+    background: "#090b1a"
+  }
+};
