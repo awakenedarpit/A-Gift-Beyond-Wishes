239ca4436fb86b9dc6ae272ba13bc5e84394fac5
awakenedarpit <awakenedarpit@gmail.com>
2026-07-22 23:34:56 +0530

    Create gallery-data.js

---
+++ gallery-data.js
@@ -0,0 +1,12 @@
+const GALLERY = [
+  {
+    image: "assets/photos/photo1.jpg",
+    title: "Beautiful Smile 😊",
+    caption: "Keep smiling. The world is brighter because of it."
+  },
+  {
+    image: "assets/photos/photo2.jpg",
+    title: "A Beautiful Moment ✨",
+    caption: "May every day bring you happiness."
+  }
+];
